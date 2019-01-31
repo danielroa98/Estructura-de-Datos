c566507c5fe28821b0006168a5d2b3b85af1b311
danielroa98 <danielroa98@gmail.com>
2019-01-31 13:50:18 -0600

    Programa visto en clase (Versión 2)

--- Recursion.java
+++ Recursion.java
@@ -1,43 +1,84 @@
-public class Recursion{
-  public double factRec(double n){
+public class Recursion {
 
-    if (n == 0){
-      return 1;
-    }
-    else {
-      return n * factRec(n-1);
-    }
-  }
+ public int[] A = {1, 9, 3, 4, 5};
 
-  public int multRec(int a, int b){
+ public double factRec(double n) { //operación recursiva para calcular el factorial de un números
 
-    if (a == 0) {
-      return 0;
-    }
-    else{
-      return b + multRec(a-1, b);
-    }
-
-  }
-
-public int potRec(int x, int n){
   if (n == 0) {
-    return 1;
+   return 1;
+  } else {
+   return n * factRec(n - 1);
+  }
+ }
+
+ public int multRec(int a, int b) { //operación recursiva para calcular la multiplicación de un número
+
+  if (a == 0) {
+   return 0;
+  } else {
+   return b + multRec(a - 1, b);
   }
 
-  else{
-    return x;
+ }
+
+ public int pot(int a, int b) { //ecuación recursiva para calcular la pot de un número
+  if (b == 0) {
+   return 1;
+  } else {
+   //return a * pot(a, b-1);  //ejemplo usando la misma sección
+   return multRec(a, pot(a, b - 1)); //ejemplo usando la operacion de multRec y pot
   }
-}
+ }
 
-  public static void main(String[] args) {
-    int res = 0;
-
-    Recursion miObjeto = new Recursion();
-
-    res = miObjeto.multRec(10, 5);
-
-    System.out.print("\nEl resultado es: ");
-    System.out.print(res+"\n");
+ public int max(int a, int b) {
+  if (a > b) {
+   return a;
+  } else {
+   return b;
   }
+ }
+
+ public int maximo(int n, int pos) {
+  if (pos == n - 1) {
+   return A[pos];
+  } else {
+   return max(A[pos], maximo(n, pos + 1));
+  }
+ }
+
+ public int vowels(String s) {
+
+  if (s.length() == 0) {
+   return 0;
+  } else if (s.charAt(0) == 'i') {
+   return 1 + vowels(s.substring(1));
+  } else if (s.charAt(0) == 'o') {
+   return 1 + vowels(s.substring(1));
+  } else if (s.charAt(0) == 'u') {
+   return 1 + vowels(s.substring(1));
+  } else if (s.charAt(0) == 'e') {
+   return 1 + vowels(s.substring(1));
+  } else if (s.charAt(0) == 'a') {
+   return 1 + vowels(s.substring(1));
+  } else {
+   return 0 + vowels(s.substring(1));
+  }
+ }
+
+ public int fibonacci(int n) {
+
+ }
+
+ public static void main(String[] args) {
+  int res = 0;
+
+  String s;
+
+  Recursion miObjeto = new Recursion();
+
+  res = miObjeto.vowels("¨Prueba de vocales");
+
+  System.out.print("\nEl resultado es: ");
+  System.out.print(res + " vocales\n");
+ }
 }
